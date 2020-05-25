dac9de7e1560473968b44652be7b04c083872036
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2020-05-25 09:07:00 +0000

    refactor and add comments

--- mizu.ml
+++ mizu.ml
@@ -4,6 +4,9 @@ type action =
   | Post of bytes list * nat list
   | Poke of address
 
+(* Timestamps impose a total ordering on all messages, as Tezos should
+ * guarantee strict monotonicity.
+ * TODO: is this true? can we rely on this? *)
 type message =
   { content : bytes
   ; timestamp : timestamp
@@ -16,33 +19,47 @@ type user_data =
 
 type storage = (address, user_data) big_map
 
+let is_empty : nat list -> bool = function
+  | [] -> true
+  | _ -> false
+;;
+
+let incr (n : nat) : nat = n +^ Nat 1
+
 let post (add : bytes list) (remove : nat list) (storage : storage) =
   let sender = Global.get_sender () in
   let timestamp = Global.get_now () in
   let new_messages = List.map (fun content -> { content; timestamp }) add in
   let new_user_data =
+    (* You can only post to your own postal box *)
     match BigMap.get sender storage with
     | None ->
-      assert (List.length remove = Nat 0);
+      assert (is_empty remove);
       { postal_box = new_messages; pokes = [] }
     | Some user_data ->
       let _, remaining_indices, postal_box =
+        (* We assume here that [remove] is sorted in ascending order,
+         * so all elements are actually removed. *)
         List.fold_left
           (fun (index, rm_indices, accum) element ->
             match rm_indices with
-            | [] -> index +^ Nat 1, [], element :: accum
-            | x :: xs when index = x -> index +^ Nat 1, xs, accum
-            | _ -> index +^ Nat 1, rm_indices, element :: accum)
+            | [] -> incr index, [], element :: accum
+            | x :: xs when index = x -> incr index, xs, accum
+            | _ -> incr index, rm_indices, element :: accum)
           (Nat 0, remove, new_messages)
           user_data.postal_box
       in
-      assert (List.length remaining_indices = Nat 0);
+      (* [remaining_indices] should be empty, which would not be the case
+       * if [remove] was not given in ascending order or has elements
+       * greater or equal to the length of [user_data.postal_box] *)
+      assert (is_empty remaining_indices);
       { user_data with postal_box }
   in
   ([] : operation list), BigMap.update sender (Some new_user_data) storage
 ;;
 
 let poke (address : address) (storage : storage) =
+  (* Anybody can poke anybody else *)
   match BigMap.get address storage with
   | None -> failwith "invalid address"
   | Some user_data ->
